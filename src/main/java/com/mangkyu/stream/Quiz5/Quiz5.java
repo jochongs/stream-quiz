334a8a9831bbb91e218a854130b5293aeec71a0c
jochongs <joch2712@naver.com>
2024-06-21 00:26:12 +0900

    solve 5-3

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,8 +1,8 @@
 package com.mangkyu.stream.Quiz5;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Quiz5 {
 
@@ -18,17 +18,29 @@ public class Quiz5 {
 
     // 5.2 문자열 중에서 가장 긴 것의 길이를 출력하시오.
     public int quiz2() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .map(str -> str.length())
+                .max(Comparator.comparing(n -> n))
+                .orElse(0);
     }
 
     // 5.3 임의의 로또번호(1~45)를 정렬해서 출력하시오.
     public List<Integer> quiz3() {
-        return Collections.emptyList();
+        return new Random().ints(1, 46)
+                .distinct()
+                .limit(6)
+                .boxed() // ????
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     // 5.4 두 개의 주사위를 굴려서 나온 눈의 합이 6인 경우를 모두 출력하시오.
     public List<Integer[]> quiz4() {
-        return Collections.emptyList();
+        return IntStream.rangeClosed(1, 6)
+                .boxed()
+                .flatMap(num1 -> IntStream.rangeClosed(1, 6).boxed().map(num2 -> new Integer[]{num1, num2}))
+                .filter(elem -> elem[0] + elem[1] == 6)
+                .collect(Collectors.toList());
     }
 
 }
